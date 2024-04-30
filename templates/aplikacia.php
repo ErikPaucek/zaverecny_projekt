d761f04fd6b1a42d892cfb25486f0eaf82b36e64
Erik Pauček <erik.paucek2002@gmail.com>
2024-04-30 15:24:25 +0200

    rename

--- templates/aplikacia.php
+++ templates/aplikacia.php
@@ -15,7 +15,7 @@ include_once "header_footer/header.php";
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <!--MENU-->
+    
     
     
     <!--main-(jadro)-->
